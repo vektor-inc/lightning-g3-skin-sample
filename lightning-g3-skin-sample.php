d8f1fa5a0b993e7d0aaaa892dc1105571f711986
kurudrive <kurudrive@gmail.com>
2021-03-18 22:51:21 +0900

    コメント追加のみ

--- lightning-g3-skin-sample.php
+++ lightning-g3-skin-sample.php
@@ -24,11 +24,14 @@ $data = get_file_data( __FILE__, array( 'version' => 'Version' ) );
 
 add_filter( 'lightning_g3_skins', 'ltg3_add_skin_sample' );
 function ltg3_add_skin_sample( $skins ){
+    // sample の部分が識別名です。好きな名前に変更してください。
     $skins['sample'] = [
-        'label'           => __( 'Sample Skin G3', 'lightning' ),
+        // label が Lightning デザイン設定 のスキン選択プルダウンに表示される名称
+        'label'           => __( 'Sample Skin G3', 'lightning' ),   
         'css_url'         => plugin_dir_url( __FILE__ ) . '/css/style.css',
         'css_path'     	  => plugin_dir_path( __FILE__ ) . '/css/style.css',
         'editor_css_url'  => plugin_dir_url( __FILE__ ) . '/css/editor.css',
+        // スキン固有の処理を入れる場合（非推奨）
         'php_path'        => plugin_dir_path( __FILE__ ) . '/functions.php',
         'js_path'         => '',
         'version'         => $data['version'],
